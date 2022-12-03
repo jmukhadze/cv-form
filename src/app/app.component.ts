6c148d73899e8acb465d06af1b13ea9b9dc89c1a
jmukhadze <119632994+jmukhadze@users.noreply.github.com>
2022-12-03 12:38:07 +0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  form: FormGroup = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required,
+      Validators.email]),
+    UserControlInfo: new FormGroup({
+      phone: new FormControl('', [Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(15),
+        Validators.pattern('[0-9]+')]),
+
+      age: new FormControl('', Validators.required),
+      year: new FormControl('', Validators.required),
+      street: new FormControl('', Validators.required),
+      education: new FormControl('', Validators.required),
+      work: new FormControl('', Validators.required),
+      link1: new FormControl('', Validators.required),
+      link2: new FormControl('', Validators.required),
+    }),
+    active: new FormControl(true, Validators.requiredTrue),
+  });
+
+  submit() {
+    console.log(this.form)
+  }
+}
